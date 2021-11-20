d82fa22a00c593653100236d2e65574f0dc2fc85
Dan Stuart <dan.stuart@deptagency.com>
2021-11-20 20:57:15 +0000

    adding gtm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
-import type { AppProps } from 'next/app'
-import '../styles/globals.css'
+import { useEffect } from "react";
+import type { AppProps } from "next/app";
+import TagManager from "react-gtm-module";
 
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: "GTM-P46NKT8",
+    });
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
